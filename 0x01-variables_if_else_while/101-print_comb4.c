46f0a809835ccee8d6e98aadc565d6f4ac2bc131
Debb07 <latonadeborah@gmail.com>
2022-09-11 15:30:57 -0700

    Print combinations

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,32 +10,29 @@
 
 int main(void)
 {
-	int i, j, k;
+	int n, m, l;
 
-	i = 48;
-	j = 48;
-	k = 48;
-	while (i < 58)
+	for (n = 48; n < 58; n++)
 	{
-		j = i + 1;
-		while (j < 58)
+		for (m = 49; m < 58; m++)
 		{
-			k = j + 1;
-			while (k < 58)
+			for (l = 50; l < 58; l++)
 			{
-				putchar(i);
-				putchar(j);
-				putchar(k);
-				if (i < 55 || j < 56 || k < 57)
+				if (l > m && m > n)
 				{
-					putchar(44);
-					putchar(32);
-					k++;
+					putchar(n);
+					putchar(m);
+					putchar(l);
+					if (n != 55 || m != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
 				}
-				j++
 			}
-			i++
 		}
-		putchar(10);
-		return (0);
 	}
+	putchar('\n');
+	return (0);
+}
+
