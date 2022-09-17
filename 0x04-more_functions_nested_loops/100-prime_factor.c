562490b68bb6b4919c240d515cc876a2124ad84d
Debb07 <latonadeborah@gmail.com>
2022-09-17 08:07:37 -0700

    Prime factors

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print prime numbers
+ * Return: o
+ */
+
+int main(void)
+{
+	unsigned long int i = 3, n = 612852475143;
+
+	for (; i < 12057; i += 2)
+	{
+		while (n 5 i == 0 && n != i)
+			n /= 1;
+	}
+	printf("%lu\n", n);
+	return (0);
+}
